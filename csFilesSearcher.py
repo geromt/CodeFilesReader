b074956acfa4d9724d371de11015bd038612dc1b
geromt <gerardomartinez205@gmail.com>
2024-11-13 21:50:32 -0600

    feat: First commit. Implementation of find_files_with_extension that search for files with a given extension and return them as a dict

---
+++ csFilesSearcher.py
@@ -0,0 +1,31 @@
+import os
+import pprint
+
+main_path = "C:\\Users\\SPARTAN PC\\wkspaces\\PoseTrackerTest\\Assets"
+extension = ".cs"
+
+
+def find_files_with_extension(path, extension):
+    files_found = {}
+    files_list = []
+    for f in os.listdir(path):
+        abspath = os.path.join(path, f)
+        if os.path.isdir(abspath):
+            files_dict = find_files_with_extension(abspath, extension)
+            if files_dict:
+                if path in files_found:
+                    files_found[path].append(files_dict)
+                else:
+                    files_found[path] = [files_dict]
+        elif os.path.isfile(abspath):
+            name, ext = os.path.splitext(f)
+            if ext == extension:
+                files_list.append(f)
+
+    if files_list:
+        files_found[path] = files_list
+    return files_found
+
+
+if __name__ == "__main__":
+    pprint.pprint(find_files_with_extension(main_path, extension))
